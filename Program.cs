4c157a8849f81471c6e1a4b51272cdbe8755ef20
Ivan <375291073707ls@gmail.com>
2022-09-10 10:54:44 +0300

    заполняем массив методом FillArray

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/* Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+Примеры:
+["hello", "2", "world", ":-)"]->["2", ":-)"]*/
+
+int size = 4;
+string[] strArray = new string[size];
+int maxLength = 3;
+
+FillArray(strArray);
+Console.WriteLine("Массив");
+PrintArray(strArray);
+
+int rusultArraySize = SizeResultArray(strArray, maxLength);
+string[] resultArray = new string[resultArraySize];
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} строку: ");
+        array[i] = Console.ReadLine();
+    }
+}
+
